f18de98026dfc98346144af8628d6961b09943f9
Joshua Scott <jscott313@gmail.com>
2018-01-25 22:41:26 +0200

    Navbar is now responsive and resizes on change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,13 @@
 <template>
   <b-container role="navigation">
-    <b-nav justified tabs>
+    <b-nav
+      justified
+      :tabs="width > 500"
+      :vertical="width <= 500"
+    >
       <b-nav-item to="/" exact  >Home</b-nav-item>
       <b-nav-item to="/products">Products</b-nav-item>
-      <b-nav-item to="/rd"      >Research & Development</b-nav-item>
+      <b-nav-item to="/rd"      >R & D</b-nav-item>
       <b-nav-item to="/about"   >About/Contact</b-nav-item>
     </b-nav>
   </b-container>
@@ -11,10 +15,28 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      width: window.outerWidth
+    }
+  },
+  methods: {
+    handleResize () {
+      this.width = window.outerWidth
+    }
+  },
+  beforeMount () {
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.handleResize)
+  }
 }
 </script>
 
 <style scoped>
-
+  .active.nav-link {
+    font-weight: 700;
+    color: #495057;
+  }
 </style>
