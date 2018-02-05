e8e8deace7816a82cb76b0e7fd5e86c59ecad058
Joshua Scott <jscott313@gmail.com>
2018-02-05 20:27:45 +0200

    Fix indentation

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,58 +1,62 @@
-<template lang="pug">
+<template>
   <b-container class="footer">
-  <b-jumbotron bg-variant="info" text-variant="dark">
-    <b-row>
-    <b-col>
-    <h1 class="title">CONTACT INFORMATION</h1>
-    </b-col>
-    <b-col>
-    <h2 class="hours">OPENING HOURS</h2>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://www.facebook.com/renotechoy/" target="_blank">
-    <b-img src="@/assets/facebook.svg"/></a>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://twitter.com/" target="_blank">
-    <b-img src="@/assets/twitter.svg"/></a>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://www.instagram.com/" target="_blank">
-    <b-img src="@/assets/instagram.svg"/></a>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://www.linkedin.com/" target="_blank">
-    <b-img src="@/assets/linkedin.svg"/></a>
-    </b-col>
-    </b-row>
+    <b-jumbotron bg-variant="info" text-variant="dark">
 
-    <b-row class="time">
-    <b-col>Renotech Oy</b-col>
-    <b-col>Mon - Fri 8:00-16:00</b-col>
-    <b-col></b-col>
-    </b-row>
+      <b-row>
+        <b-col>
+          <h1 class="title">CONTACT INFORMATION</h1>
+        </b-col>
+        <b-col>
+          <h2 class="hours">OPENING HOURS</h2>
+        </b-col>
+        <b-col class="media-logo">
+          <a href="https://www.facebook.com/renotechoy/" target="_blank">
+          <b-img src="@/assets/facebook.svg"/></a>
+        </b-col>
+        <b-col class="media-logo">
+          <a href="https://twitter.com/" target="_blank">
+          <b-img src="@/assets/twitter.svg"/></a>
+        </b-col>
+        <b-col class="media-logo">
+          <a href="https://www.instagram.com/" target="_blank">
+          <b-img src="@/assets/instagram.svg"/></a>
+        </b-col>
+        <b-col class="media-logo">
+          <a href="https://www.linkedin.com/" target="_blank">
+          <b-img src="@/assets/linkedin.svg"/></a>
+        </b-col>
+      </b-row>
 
-    <b-row>
-    <b-col>Sampsankatu 4 B 20520</b-col>
-    </b-row>
-    <b-row>
-    <b-col>TURKU FINLAND</b-col>
-    </b-row>
-    <b-row>
-    <b-col>+358 40 485 1550/+358 50 558 1806</b-col>
-    </b-row>
+      <b-row class="time">
+        <b-col>Renotech Oy</b-col>
+        <b-col>Mon - Fri 8:00-16:00</b-col>
+        <b-col></b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>Sampsankatu 4 B 20520</b-col>
+      </b-row>
+      <b-row>
+        <b-col>TURKU FINLAND</b-col>
+      </b-row>
+      <b-row>
+        <b-col>+358 40 485 1550/+358 50 558 1806</b-col>
+      </b-row>
+
     </b-jumbotron>
-    </b-container>
+  </b-container>
 </template>
 
 <script>
 export default {
+
 }
 </script>
 
 <style lang="scss" scoped>
   @import '~@/styles/_variables.scss';
-.footer {
+
+  .footer {
     margin-top: 20px;
     margin-bottom: 20px;
     color: #495057;
