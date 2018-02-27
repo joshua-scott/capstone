014ed41361ce50c3134df6a9ad78b2d894d5e6d5
Mala Shrestha <mala.shrestha@edu.turkuamk.fi>
2018-02-27 12:15:16 +0200

    Completed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,44 +3,44 @@
   <hr class="hr">
   <b-card text-variant="dark">
     <b-row>
-    <b-col>
-    <h1 class="title">CONTACT INFORMATION</h1>
-    </b-col>
-    <b-col>
-    <h2 class="hours">OPENING HOURS</h2>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://www.facebook.com/renotechoy/" target="_blank">
-    <b-img src="@/assets/facebook.svg"/></a>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://twitter.com/" target="_blank">
-    <b-img src="@/assets/twitter.svg"/></a>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://www.instagram.com/" target="_blank">
-    <b-img src="@/assets/instagram.svg"/></a>
-    </b-col>
-    <b-col class="media-logo">
-    <a href="https://www.linkedin.com/" target="_blank">
-    <b-img src="@/assets/linkedin.svg"/></a>
-    </b-col>
+      <b-col>
+        <h1 class="title">CONTACT INFORMATION</h1>
+      </b-col>
+      <b-col>
+        <h2 class="hours">OPENING HOURS</h2>
+      </b-col>
+      <b-col class="media-logo">
+        <a href="https://www.facebook.com/renotechoy/" target="_blank">
+        <b-img src="@/assets/facebook.svg"/></a>
+      </b-col>
+      <b-col class="media-logo">
+        <a href="https://twitter.com/" target="_blank">
+      <b-img src="@/assets/twitter.svg"/></a>
+      </b-col>
+      <b-col class="media-logo">
+        <a href="https://www.instagram.com/" target="_blank">
+        <b-img src="@/assets/instagram.svg"/></a>
+      </b-col>
+      <b-col class="media-logo">
+        <a href="https://www.linkedin.com/" target="_blank">
+        <b-img src="@/assets/linkedin.svg"/></a>
+      </b-col>
     </b-row>
 
     <b-row>
-    <b-col>Renotech Oy</b-col>
-    <b-col class="time">Mon - Fri 8:00-16:00</b-col>
-    <b-col></b-col>
+      <b-col>Renotech Oy</b-col>
+      <b-col class="time">Mon - Fri 8:00-16:00</b-col>
+      <b-col></b-col>
     </b-row>
 
     <b-row>
-    <b-col>Sampsankatu 4 B 20520</b-col>
+      <b-col>Sampsankatu 4 B 20520</b-col>
     </b-row>
     <b-row>
-    <b-col>TURKU FINLAND</b-col>
+      <b-col>TURKU FINLAND</b-col>
     </b-row>
     <b-row>
-    <b-col>+358 40 485 1550/+358 50 558 1806</b-col>
+      <b-col>+358 40 485 1550/+358 50 558 1806</b-col>
     </b-row>
     </b-card>
     </b-container>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@ import Carousel from '@/components/Carousel.vue'
 
 export default {
   components: {
-    Carousel,
+    Carousel
   }
 }
 </script>
