ae742f776a407bf395c461d35e485653d5579721
ishustov <ilja.shustov@itcollege.ee>
2018-03-12 23:32:16 +0200

    content added

--- src/views/ResearchDevelopment.vue
+++ src/views/ResearchDevelopment.vue
@@ -1,27 +1,27 @@
 <template lang="pug">
-  b-container
-      b-alert(variant="success" show style="margin-top: 30px;") This will be the R&D page
-      p(style="color: red;")
-        strong Here's some random sample data for now, to demonstrate how we can get data in the vuex store and show it:
-      div(v-for="post in posts")
-        h4 {{ post.title }}
-        p {{ post.body }}
+<div>
+    <div>
+    <rdcarousel></rdcarousel>
+    </div>
+    <div>
+    <articles></articles>
+    </div>
+    b-container
+        b-alert(variant="success" show style="margin-top: 30px;") This will be the R&D page
+        p(style="color: red;")
+          strong Here's some random sample data for now, to demonstrate how we can get data in the vuex store and show it:
+        div(v-for="post in posts")
+</div>
 </template>
 
 <script>
+
+import Rdcarousel from '@/components/rdcarousel.vue'
+import Articles from '@/components/Articles.vue'
+
 export default {
-  data () {
-    return {
-      alertText: 'This will be the Research and Development page'
-    }
-  },
-  computed: {
-    posts () {
-      return this.$store.state.otherpageData.data
-    },
-    products () {
-      return this.$store.state.products
-    }
+  components: {
+    Rdcarousel, Articles
   }
 }
 </script>
