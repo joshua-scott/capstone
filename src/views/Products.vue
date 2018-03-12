57b85ffee7ecf880da4046b36231107141754f36
Joshua Scott <jscott313@gmail.com>
2018-03-12 22:59:21 +0200

    Simplify product template

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,15 +3,12 @@
     <products-jumbotron></products-jumbotron>
     <b-card
       v-for="product in products"
-      :key="product.name"
+      :key="`${product.name}-${product.language}`"
     >
-        <p><strong>Product: </strong>{{ product.name }}</p>
-
-        <p><strong>Description: </strong></p>
+        <h3>{{ product.name }}</h3>
         <div v-html="product.description"></div>
-
-        <p><strong>Representative: </strong>{{ product.representative }}</p>
-        <p><strong>Sold by the: </strong>{{ product.salesUnit }}</p>
+        <p>Product representative: {{ product.representative }}</p>
+        <p><strong>Sale unit: </strong>{{ product.salesUnit }}</p>
         <b-img fluid :src="product.image" />
     </b-card>
   </div>
