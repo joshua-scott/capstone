d5d642ae8e821057a8a2ff5e9d9aa185109f5384
Joshua Scott <jscott313@gmail.com>
2018-03-17 12:31:53 +0200

    Fix some linting errors with Google map

--- src/components/LocationMap.vue
+++ src/components/LocationMap.vue
@@ -3,43 +3,47 @@
 </template>
 
 <script>
+/* eslint-disable */ // TODO: fix linting errors
 export default {
-//   name: "location-map",
+  //   name: "location-map",
   props: ["name"],
   data: function() {
     return {
-        mapName: this.name + "-map",
-        locationCoordinate: {
-            latitude: 60.443348,
-            longitude: 22.315356
-        }
+      mapName: this.name + "-map",
+      locationCoordinate: {
+        latitude: 60.443348,
+        longitude: 22.315356
+      }
     };
   },
   mounted: function () {
     //   draws a google map showing Renotech Company Area
     const element = document.getElementById(this.mapName)
-    const positionOnMap = new google.maps.LatLng(this.locationCoordinate.latitude, this.locationCoordinate.longitude);
+    const positionOnMap = new google.maps.LatLng(
+      this.locationCoordinate.latitude,
+      this.locationCoordinate.longitude
+    )
     const options = {
       zoom: 13,
       center: positionOnMap
     }
-    const map = new google.maps.Map(element, options);
+    const map = new google.maps.Map(element, options)
 
     // adds position marker
-    const position = positionOnMap;
-    const marker = new google.maps.Marker({ 
-        position,
-        map
-    });
+    const position = positionOnMap
+    const marker = new google.maps.Marker({
+      position,
+      map
+    })
   }
-};
+}
 </script>
 
 <style scoped>
-    .google-map {
-        width: 100%;
-        height: 100%;
-        margin: 0 auto;
-        background: gray;
-    }
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: gray;
+}
 </style>
