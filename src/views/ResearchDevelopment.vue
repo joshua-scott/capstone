4481caa3b563dc0dbdb3c376841dfef020c82bb7
Joshua Scott <jscott313@gmail.com>
2018-03-17 16:19:14 +0200

    Remove fake data from R&D

--- src/views/ResearchDevelopment.vue
+++ src/views/ResearchDevelopment.vue
@@ -6,11 +6,6 @@
     <div>
     <articles></articles>
     </div>
-    b-container
-        b-alert(variant="success" show style="margin-top: 30px;") This will be the R&D page
-        p(style="color: red;")
-          strong Here's some random sample data for now, to demonstrate how we can get data in the vuex store and show it:
-        div(v-for="post in posts")
 </div>
 </template>
 
