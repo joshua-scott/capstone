9d3299c20fc4b08742c48f79e765e964ac68403e
Joshua Scott <jscott313@gmail.com>
2018-03-17 16:20:14 +0200

    Clean up store.js

--- src/store/store.js
+++ src/store/store.js
@@ -6,34 +6,22 @@ import PrismicDOM from 'prismic-dom'
 
 Vue.use(Vuex)
 
+const dev = process.env.NODE_ENV !== 'production'
+
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
+  // State is where all the data is stored
   state: {
     language: 'en-gb',
     homepageData: {},
-    otherpageData: {},
-    prismicData: {},
     products: []
   },
+  // Actions main job is to get data from somewhere else
+  // and then commit the mutations defined below
   actions: {
-    async loadAllData ({ commit }) {
-      const homepageData = await axios.get('https://jsonplaceholder.typicode.com/posts')
-      const otherpageData = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    async getHomepageData ({ commit }) {
+      const homepageData = await axios.get('https://jsonplaceholder.typicode.com/posts') // Just pretend data for now
       commit('setHomepageData', homepageData)
-      commit('setOtherpageData', otherpageData)
-    },
-    async loadPrismic ({ commit }) {
-      // https://prismic.io/docs/javascript/getting-started/integrating-with-an-existing-javascript-project
-      try {
-        const api = await Prismic.getApi('https://renotech.prismic.io/api/v2')
-        const response = await api.query('') // Empty query to get all data
-        console.log(response)
-        console.log(response.results)
-        const prismicData = response.results
-        commit('setPrismicData', prismicData)
-      } catch (err) {
-        console.log('Error on loadPrismic action', err)
-      }
     },
     async getProducts ({ commit }) {
       try {
@@ -41,28 +29,23 @@ export default new Vuex.Store({
         const response = await api.query(
           Prismic.Predicates.at('document.type', 'product'),
           { lang: '*' }
-          // { orderings: '[my.product.date desc]' }
         )
         const data = response.results
-        console.log('data:')
-        console.dir(data)
 
         let products = []
 
         data.forEach(obj => {
-          console.log('object in data:')
-          console.log({ obj })
+          if (dev) console.log({ obj })
           let product = {}
           const p = obj.data
           product.language = obj.lang
           product.name = p.product_name[0].text
           product.number = p.product_number
-          product.representative = p.product_representative
-          product.image = p.repeatable_picture_field[0].picture_1.url
           product.description = PrismicDOM.RichText.asHtml(p.product_description)
+          product.image = p.repeatable_picture_field[0].picture_1.url
+          product.representative = p.product_representative
           product.salesUnit = p.sales_unit
-
-          console.log({ product })
+          if (dev) console.log({ product })
 
           products.push(product)
         })
@@ -73,6 +56,7 @@ export default new Vuex.Store({
       }
     }
   },
+  // Mutations are the only place we actually alter the state
   mutations: {
     toggleLanguage (state) {
       state.language = state.language === 'fi' ? 'en-gb' : 'fi'
@@ -80,12 +64,6 @@ export default new Vuex.Store({
     },
     setHomepageData (state, data) {
       state.homepageData = data
-    },
-    setOtherpageData (state, data) {
-      state.otherpageData = data
-    },
-    setPrismicData (state, data) {
-      state.prismicData = data
     },
     setProducts (state, data) {
       state.products = data
