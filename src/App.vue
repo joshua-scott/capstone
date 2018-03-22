9a123cc76a39f0c999d7562362e385eba2220869
Joshua Scott <jscott313@gmail.com>
2018-03-22 19:37:32 +0200

    Fix spacing

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ import Header from '@/components/Header.vue'
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
 export default {
   name: 'App',
   data () {
