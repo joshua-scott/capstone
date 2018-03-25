dfbafdf1022ed6031e290a0ca170d6cd4405e7f5
Mala Shrestha <mala.shrestha@edu.turkuamk.fi>
2018-03-25 22:37:13 +0300

    product list size modified and border colored

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,12 +1,15 @@
 <template>
+  <b-col xs="12" sm="6" md="3">
     <b-card :title="product.name"
             :img-src="product.image"
             :img-alt="`Photograph of ${product.name}`"
             img-top
             class="product-card mb-2"
+            border-variant="primary"
     >
       <b-button :to="`/product/${product.number}`" variant="primary">Find out more</b-button>
     </b-card>
+  </b-col>
 </template>
 
 <script>
