0989b0d8f92f559ab7af289a5dbf283ee2935ae5
LanNguyen <lan.nguyen@edu.turkuamk.fi>
2018-04-17 01:27:38 +0300

    New Styling

--- src/views/ProductLine.vue
+++ src/views/ProductLine.vue
@@ -1,20 +1,44 @@
 <template>
   <b-container>
-    <b-jumbotron class="jumbotron-styling" text-variant="white" border-variant="dark">
-      <h1 class="header-padding">
+    <div class="jumbotron-styling" text-variant="white" border-variant="dark">
+      <h1>
         Title for productLine
       </h1>
       <h5>
         Some introductions
       </h5>
-    </b-jumbotron>
+    </div>
     <b-row>
       <b-col cols="9">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, natus delectus, harum neque unde necessitatibus cum sequi inventore aspernatur minus quos tenetur quae accusantium magnam enim aliquam pariatur alias sed ipsum dicta totam amet temporibus corrupti debitis! Expedita aperiam consectetur, delectus enim nobis a illo totam provident illum cupiditate aliquam sit itaque! Consequuntur quidem dicta, doloremque accusamus tenetur sunt, asperiores aut cumque amet nostrum dolorum, sapiente expedita. Id nemo, voluptatibus sed asperiores quas ipsa eaque perferendis, est distinctio, cum obcaecati!
         </p>
+        <b-row>
+          <b-col>
+            <p>Size: 19mm-22mm-25mm</p>
+          </b-col>
+          <b-col>
+            <p>Status: Available</p>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <p>Size: 19mm-22mm-25mm</p>
+          </b-col>
+          <b-col>
+            <p>Status: Available</p>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <p>Size: 19mm-22mm-25mm</p>
+          </b-col>
+          <b-col>
+            <p>Status: Available</p>
+          </b-col>
+        </b-row>
         <div>
-          <b-embed type="iframe"
+          <b-embed class="styling-embed" type="iframe"
            aspect="16by9"
            src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
            allowfullscreen>
@@ -44,7 +68,6 @@
         </b-media>
       </b-col>
     </b-row>
-    
   </b-container>
 
 </template>
@@ -56,12 +79,19 @@ export default {
 <style>
 .jumbotron-styling{
   background-color:#263779;
-  opacity:0.6;
-  max-height:200px;
+  height:125px;
+  margin-bottom:40px;
+  padding:25px;
+  padding-top:10px;
+  color:white;
+  border-radius:5px;
 }
 .header-padding{
 }
 .b-media-margin{
-  margin-bottom:5px;
+  margin-bottom:10px;
+}
+.styling-embed{
+  margin-top:30px;
 }
 </style>
