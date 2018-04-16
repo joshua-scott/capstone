adfdb722e2c3f0410299236f5a420b11c201cd15
Rjacobs15 <robin.eetu.jacobs@gmail.com>
2018-04-16 17:28:00 +0300

    Minor layour fixes to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,11 @@
       <hr>
       <div>
         <b-row>
-          <b-col sm=12 lg=6>
-          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Frenotechoy&tabs=timeline&width=450&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="450" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          <b-col class="text-center" sm=12 lg=6>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Frenotechoy&tabs=timeline&width=470&height=700&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
+          width="470" height="700" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
           </b-col>
-          <b-col sm=12 lg=6>
+          <b-col class="text-center" sm=12 lg=6>
             <b-embed type="iframe"
               aspect="16by9"
               src="https://www.youtube.com/embed/dOTttRvR2tI"
