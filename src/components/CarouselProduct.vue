9b2e1a0433ee75c4678a0a3141ea37e4fe3f5191
LanNguyen <lan.nguyen@edu.turkuamk.fi>
2018-04-23 14:10:31 +0300

    new carousel component

---
+++ src/components/CarouselProduct.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  <b-container>
+    b-carousel(
+      class="carousel"
+      controls
+      img-height="400"
+      indicators
+      :interval="5000")
+      template(v-for="(slide, index) in slides")
+        b-carousel-slide(
+          :key="index"
+          :img-src="slide.imageUrl"
+          :caption="slide.heading"
+          :text="slide.description")
+  </b-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    slides () {
+      const slides = this.$store.state.carouselItems
+      const language = this.$store.state.language
+      return slides.filter(slide => slide.language === language)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .carousel {
+    margin-top: 30px;
+    text-shadow: 1px 1px 2px #333;
+  }
+</style>
